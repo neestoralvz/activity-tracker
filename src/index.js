a70ce8fbc999ecd0a07e37ca1b5ffc19849f3116
neestoralvz <nestor.alvz@gmail.com>
2024-07-12 15:14:16 -0600

    Ya se hizo el deployment pero no carga nada

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
