8493a3c2be42ca43c177f60e1c4367330d9672f3
neestoralvz <nestor.alvz@gmail.com>
2024-07-12 14:37:23 -0600

    Fix: Añadir react-tabs y su CSS

--- src/pages/TabbedReports/TabbedReports.js
+++ src/pages/TabbedReports/TabbedReports.js
@@ -1,35 +1,29 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import Summary from '../../components/Summary/Summary';
-import Trends from '../../components/Trends/Trends';
-import ActivityPieChart from '../../components/ActivityPieChart/ActivityPieChart';
-import NavigationLinks from '../../components/Navigation/NavigationLinks';
+import 'react-tabs/style/react-tabs.css'; // Asegúrate de que esta línea está presente
 import './TabbedReports.css';
 
 const TabbedReports = ({ activities }) => {
   return (
-    <div className="tabbed-reports">
-      <h2>Reportes en Pestañas</h2>
-      <Tabs>
-        <TabList>
-          <Tab>Resumen General</Tab>
-          <Tab>Tendencias</Tab>
-          <Tab>Distribución de Actividades</Tab>
-        </TabList>
+    <Tabs>
+      <TabList>
+        <Tab>Reporte 1</Tab>
+        <Tab>Reporte 2</Tab>
+      </TabList>
 
-        <TabPanel>
-          <Summary data={activities} />
-        </TabPanel>
-        <TabPanel>
-          <Trends data={activities} />
-        </TabPanel>
-        <TabPanel>
-          <ActivityPieChart data={activities} />
-        </TabPanel>
-      </Tabs>
-      <NavigationLinks />
-    </div>
+      <TabPanel>
+        <h2>Reporte 1</h2>
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity._id}>{activity.name}</li>
+          ))}
+        </ul>
+      </TabPanel>
+      <TabPanel>
+        <h2>Reporte 2</h2>
+        <p>Contenido del reporte 2</p>
+      </TabPanel>
+    </Tabs>
   );
 };
 
